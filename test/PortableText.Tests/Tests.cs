dfbdc4bc6ecee38fb9f146ef4630cf421fc6d5db
Anders Stensaas <stensaasanders@gmail.com>
2022-02-17 08:38:19 +0100

    test: remove obsolete tests

--- test/PortableText.Tests/Tests.cs
+++ test/PortableText.Tests/Tests.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 using FluentAssertions;
 using System.IO;
-using Xunit.Abstractions;
-using System.Collections.Generic;
-using Snapshooter.Xunit;
 
 namespace PortableText
 {
-    public class YoutubeEmbed : PortableTextBlock
-    {
-        public string Title { get; set; }
-        public string Url { get; set; }
-    }
-
     public class Test
     {
         [Fact]
@@ -62,151 +52,22 @@ namespace PortableText
             result.Should().Be(null);
         }
 
-
-        [Fact]
-        public void ConvertsSlashNToBr()
-        {
-            var result = PortableTextToHtml.Render(@"
-    [
-        {
-            ""_type"": ""block"",
-            ""children"": [
-                {
-                    ""_type"": ""span"",
-                    ""text"": ""test\ntest2""
-                }
-            ],
-            ""style"": ""normal""
-        }
-    ]
-    ");
-
-            result.Should().Be("<p>test<br>test2</p>");
-        }
-
-        [Fact]
-        public void HandlesSimpleJson()
-        {
-            var json = ReadTestJsonFile("simple.json");
-            var result = PortableTextToHtml.Render(json);
-
-            result.Should().Be("<p>Very <strong>nice job</strong>!</p>");
-        }
-
-        [Fact]
-        public void HandlesLinks()
-        {
-            var json = ReadTestJsonFile("links.json");
-            var result = PortableTextToHtml.Render(json);
-
-            result.Should()
-                .Be(
-                    @"<p><strong>Spotify -> </strong><strong><a href=""https://open.spotify.com/episode/2ON1aZSJvYieU8Pewz7yUH?si=KARaXtuaQDaOkeyxs0f7OQ"">Lytt her!</a></strong></p>");
-        }
-
         [Fact]
         public void GivenNoCustomSerializers_AndCustomObjectsArePresent_ShouldNotCrash()
         {
-            var json = ReadTestJsonFile("customobjects.json");
+            var json = @"
+[
+    {
+        ""_key"": ""88778f6b7024"",
+        ""_type"": ""youtubeEmbed"",
+        ""title"": ""Top 10 goals Jon Dahl Tomasson"",
+        ""url"": ""https://youtu.be/8d9vXiGrYck""
+    }
+]
+";
             var result = PortableTextToHtml.Render(json);
 
             result.Should().Be(null);
-        }
-
-        [Fact]
-        public void GivenCustomSerializers_AndCustomObjectsArePresent()
-        {
-            var json = ReadTestJsonFile("customobjects.json");
-            var result = PortableTextToHtml.Render(json, new PortableTextSerializers
-            {
-                TypeSerializers = new Dictionary<string, TypeSerializer>
-                {
-                    {
-                        "youtubeEmbed", new TypeSerializer
-                        {
-                            Type = typeof(YoutubeEmbed),
-                            Serialize = (block, serializers) =>
-                            {
-                                var typedBlock = block as YoutubeEmbed;
-                                if (typedBlock == null)
-                                {
-                                    return string.Empty;
-                                }
-
-                                return $@"<iframe title=""{typedBlock.Title}"" href=""{typedBlock.Url}""></iframe>";
-                            }
-                        }
-                    }
-                }
-            });
-
-            result.Should()
-                .Be(
-                    @"<iframe title=""Top 10 goals Jon Dahl Tomasson"" href=""https://youtu.be/8d9vXiGrYck""></iframe>");
-        }
-
-        [Fact]
-        public void RendersNestedList()
-        {
-            var result = PortableTextToHtml.Render(ReadTestJsonFile("list.json"));
-            SnapshotExtensions.MatchFormattedHtml(result);
-        }
-
-        [Fact]
-        public void CanCustomizeListTypes()
-        {
-            var result = PortableTextToHtml.Render(ReadTestJsonFile("list.json"), new PortableTextSerializers
-            {
-                ListSerializers = new()
-                {
-                    {
-                        "bullet",
-                        listItems => $@"<ul style=""list-style: square;"">{string.Join(string.Empty, listItems)}</ul>"
-                    }
-                }
-            });
-
-            SnapshotExtensions.MatchFormattedHtml(result);
-        }
-
-        [Fact]
-        public void CanCustomizeListItemTypes()
-        {
-            var result = PortableTextToHtml.Render(ReadTestJsonFile("list.json"), new PortableTextSerializers
-            {
-                ListItemSerializers = new()
-                {
-                    { "bullet", () => (@"<li style=""list-style-type: circle;"">🎱 ", "</li>") }
-                }
-            });
-
-            SnapshotExtensions.MatchFormattedHtml(result);
-        }
-
-        [Fact]
-        public void RendersMultipleLevelListCorrectly()
-        {
-            var result = PortableTextToHtml.Render(ReadTestJsonFile("multiple-level-list.json"));
-            SnapshotExtensions.MatchFormattedHtml(result);
-        }
-
-        private static string ReadTestJsonFile(string filename)
-        {
-            return File.ReadAllText($"../../../data/{filename}");
-        }
-    }
-
-    public static class SnapshotExtensions
-    {
-        public static void MatchFormattedHtml(string html)
-        {
-            var parser = new AngleSharp.Html.Parser.HtmlParser();
-            using var document = parser.ParseDocument(html);
-            using var sw = new StringWriter();
-            var formatter = new AngleSharp.Html.PrettyMarkupFormatter
-                { Indentation = "  ", NewLine = Environment.NewLine };
-            document.ToHtml(sw, formatter);
-            Snapshot.Match(sw.ToString());
         }
     }
 }
